ada53893147a91ffe74c7f08568d28b1884e7917
Rahulx911 <89407171+Rahulx911@users.noreply.github.com>
2024-09-03 00:07:22 +0530

    Add files via upload

---
+++ DASH/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Medicine Batch-1',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Consultation',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Blood Test',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
